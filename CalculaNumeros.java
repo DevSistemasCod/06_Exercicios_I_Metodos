3a74754f9035092ff7c5ca5d7ea3a90165489ea1
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-25 23:19:54 -0300

    Update CalculaNumeros.java

--- CalculaNumeros.java
+++ CalculaNumeros.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CalculaNumeros {
 	
-	public static int calcularSoma(int soma, int numero) {
+    public static int calcularSoma(int soma, int numero) {
         return soma + numero;
     }
 
@@ -28,39 +28,38 @@ public class CalculaNumeros {
         return Math.min(menor, numero);
     }
 	
-	public static void main(String[] args) {
-	        int numero = 0, soma = 0, quantidade = 0;
-	        int maior = Integer.MIN_VALUE;
-	        int menor = Integer.MAX_VALUE;
-	        double media=0, mediaFinal; 
+    public static void main(String[] args) {
+	int numero = 0, soma = 0, quantidade = 0;
+	int maior = Integer.MIN_VALUE;
+	int menor = Integer.MAX_VALUE;
+	double media=0, mediaFinal; 
 	        
-	        Scanner lerDados = new Scanner(System.in);
+	Scanner lerDados = new Scanner(System.in);
 	        
-	        while (numero != -1) {
-	            System.out.print("Digite um número (ou -1 para sair): ");
-	            numero = lerDados.nextInt();
-
-	            if (numero != -1) {
-	                soma = calcularSoma(soma, numero);
-	                quantidade = calcularQuantidade(quantidade);
-	                maior = calcularMaior(maior, numero);
-	                menor = calcularMenor(menor, numero);
-	                media = calcularMedia(soma, quantidade);
-	            }
-	        }
+	while(numero != -1){
+	   System.out.print("Digite um número (ou -1 para sair): ");
+	   numero = lerDados.nextInt();
+          
+	   if(numero != -1){
+	      soma = calcularSoma(soma, numero);
+	      quantidade = calcularQuantidade(quantidade);
+	      maior = calcularMaior(maior, numero);
+	      menor = calcularMenor(menor, numero);
+	      media = calcularMedia(soma, quantidade);
+	   }
+        }
 	        
-
-	        mediaFinal = calcularMedia(soma, quantidade);
-
-	        System.out.println("\n -- Resultados finais --");
-	        System.out.println("Soma: " + soma);
-	        System.out.println("Quantidade: " + quantidade);
-	        System.out.println("Média: " + mediaFinal);
-	        System.out.println("Maior número: " + maior);
-	        System.out.println("Menor número: " + menor);
+	mediaFinal = calcularMedia(soma, quantidade);
+           
+	System.out.println("\n -- Resultados finais --");
+	System.out.println("Soma: " + soma);
+	System.out.println("Quantidade: " + quantidade);
+	System.out.println("Média: " + mediaFinal);
+	System.out.println("Maior número: " + maior);
+	System.out.println("Menor número: " + menor);
 	        
-	        lerDados.close();
-	    }
+        lerDados.close();
+    }
 
 
 }
