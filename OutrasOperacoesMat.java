e40bde2923b692129d1f6f1c8cc57ab38d958203
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-25 22:50:10 -0300

    Update OutrasOperacoesMat.java

--- OutrasOperacoesMat.java
+++ OutrasOperacoesMat.java
@@ -4,35 +4,34 @@ import java.util.Scanner;
 
 public class OutrasOperacoesMat {
 	
-	public static int calcularFatorial(int num) {
-		int  fatorial = 1;
+       public static int calcularFatorial(int num) {
+       		int  fatorial = 1;
 		
-		if (num < 0) {
-            System.out.println("Não é possível calcular o fatorial de um número negativo.");
-        }
-        
-        for (int i = 1; i <= num; i++) {
-            fatorial *= i;
-        }
-        
-        return fatorial;
-    }
+		if(num < 0){
+                    System.out.println("Não é possível calcular o fatorial de um número negativo.");
+                }
+                 
+                for(int i = 1; i <= num; i++){
+                   fatorial = fatorial*i;
+                }
+       
+          return fatorial;
+       }
 
-    public static int calcularRaiz(int num) {
-        if (num < 0) {
+       public static int calcularRaiz(int num) {
+             if (num < 0) {
         	System.out.println("Não é possível calcular a raiz quadrada de um número negativo.");
-        }
-        return ((int) Math.sqrt(num));
-    }
+             }
+             return ((int) Math.sqrt(num));
+       }
 
-    public static int calcularExponencial(int num) {
-        return ((int) Math.exp(num));
-    }
-	
-	public static void main(String[] args) {
-		int fatorial, raiz, exponencial, num;
-		
-		Scanner lerTeclado = new Scanner(System.in);
+       public static int calcularExponencial(int num) {
+            return ((int) Math.exp(num));
+       }
+       	
+       public static void main(String[] args) {
+	    int fatorial, raiz, exponencial, num;	
+	    Scanner lerTeclado = new Scanner(System.in);
 
 	    System.out.print("Digite um número: ");
 	    num = lerTeclado.nextInt();
